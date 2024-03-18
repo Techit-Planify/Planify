7f12f0e89bebb81309baa49ed417de564042c543
UJIN901 <dalvit9655@gmail.com>
2024-03-18 12:59:05 +0900

    feat: NotProd, member controller 추가

---
+++ src/main/java/com/ll/planify/domain/member/member/controller/MemberController.java
@@ -0,0 +1,11 @@
+package com.ll.planify.domain.member.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/members")
+public class MemberController {
+}
