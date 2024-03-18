225b6cce0b271debfe98e51ba4498a06595c95c4
UJIN901 <dalvit9655@gmail.com>
2024-03-18 12:55:53 +0900

    feat: security 관련 코드 추가
    admin으로 강제 로그인

---
+++ src/main/java/com/ll/planify/global/security/CustomUserDetails.java
@@ -0,0 +1,72 @@
+package com.ll.planify.global.security;
+
+import com.ll.planify.domain.member.member.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    private final Member member;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_MEMBER"));
+
+        if ("admin".equals(this.member.getEmail())) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        }
+
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getUsername();
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    public String getEmail(){
+        return member.getEmail();
+    }
+
+    public String getNickname(){
+        return member.getNickname();
+    }
+
+    public String getImageUrl(){
+        return member.getImageUrl();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+
