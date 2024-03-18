3139f9df42b9cfed62b4b288fd10356265ebb4ae
UJIN901 <dalvit9655@gmail.com>
2024-03-18 12:43:37 +0900

    feat: Member 관련 코드 추가
    entity, repository,  service, dto 추가
    service, dto는 회원가입 관련 코드 추가

---
+++ src/main/java/com/ll/planify/domain/member/member/entity/Member.java
@@ -0,0 +1,38 @@
+package com.ll.planify.domain.member.member.entity;
+
+import com.ll.planify.global.jpa.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
+@Builder
+@Getter
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String imageUrl;
+
+}
