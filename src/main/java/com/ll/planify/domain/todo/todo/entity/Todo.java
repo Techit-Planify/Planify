047b331372ad01f096d19b4d40d17d78071077e8
T189216 <tus0926@naver.com>
2024-03-21 17:39:04 +0900

    Todolist/crud (#5)
    * chore: init project
    
    * chore: yml, gitignore 설정 추가
    
    * feat: front 생성
    
    * feat: BaseEntity 추가
    
    * feat: Member 관련 코드 추가
    
    entity, repository,  service, dto 추가
    service, dto는 회원가입 관련 코드 추가
    
    * feat: security 관련 코드 추가
    
    admin으로 강제 로그인
    
    * feat: NotProd, member controller 추가
    
    * style: name 카멜케이스로 변경
    
    * [test] admin login
    
    * work 1
    
    * 할 일 생성 테스트
    
    * work 1
    
    * feat: 메인 페이지에서 할 일 조회
    
    * feat: 할 일 등록, 조회
    
    * feat: 할 일 수정
    
    * CURD 구현 완료, 회원 서비스와 연동 X
    
    * front remove
    
    * 충돌 수정
    
    ---------
    
    Co-authored-by: UJIN901 <dalvit9655@gmail.com>

---
+++ src/main/java/com/ll/planify/domain/todo/todo/entity/Todo.java
@@ -0,0 +1,34 @@
+package com.ll.planify.domain.todo.todo.entity;
+
+import com.ll.planify.global.jpa.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Todo extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "todo_id")
+    private Long id;
+
+    private String content;
+
+    private LocalDate deadline; // 마감일 설정
+
+    @Enumerated(EnumType.STRING)
+    private TodoPriority priority; // 우선순위 [HIGH, MEDIUM, LOW]
+
+    @Enumerated(EnumType.STRING)
+    private TodoStatus status; // 완료 상태 [PROGRESS, DONE]
+
+    @ManyToMany(mappedBy = "todos", cascade = CascadeType.ALL) // ?
+    private List<Category> categories = new ArrayList<>();
+}
