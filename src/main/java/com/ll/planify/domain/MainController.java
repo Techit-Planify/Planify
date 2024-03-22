4e8eaa492f1775a56dfaa4fa235eb60839ecbdcb
seoyeon <tus0926@naver.com>
2024-03-22 17:20:36 +0900

    feat: 메인페이지에서 해시태그 확인 가능

--- src/main/java/com/ll/planify/domain/MainController.java
+++ src/main/java/com/ll/planify/domain/MainController.java
@@ -1,15 +1,28 @@
 package com.ll.planify.domain;
 
+import com.ll.planify.domain.todo.todo.entity.Keyword;
+import com.ll.planify.domain.todo.todo.service.KeywordService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class MainController {
 
+    private final KeywordService keywordService;
+
     @RequestMapping("/")
-    public String main() {
+    public String main(Model model) {
+
+        List<Keyword> keywords = keywordService.getAllKeywords();
+        model.addAttribute("keywords", keywords);
+
         log.info("main controller");
         return "domain/main";
     }
--- src/main/java/com/ll/planify/domain/todo/todo/service/KeywordService.java
+++ src/main/java/com/ll/planify/domain/todo/todo/service/KeywordService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -31,5 +32,9 @@ public class KeywordService {
         keywordRepository.save(keyword);
 
         return keyword;
+    }
+
+    public List<Keyword> getAllKeywords() {
+        return keywordRepository.findAll();
     }
 }
