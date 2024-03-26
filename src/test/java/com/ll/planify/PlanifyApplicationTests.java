8e4bfc30091cca76afd10ddde3dd033d4e762b99
UJIN901 <57745105+UJIN901@users.noreply.github.com>
2024-03-26 13:50:25 +0900

    Feature/member (#14)
    * fix: 타임리프 함수 수정
    
    * fix: 템플릿 수정
    
    * feat: Id 얻는 함수 추가
    
    * feat: 접근권한 설정
    
    * feat: 회원정보 조회 추가
    
    * style: 공통 헤더 적용
    
    * feat: 세션 관련 추가
    
    * chore: 히든 메소드 적용
    
    * style: 로그인&로그아웃, 마이페이지 버튼 추가
    
    * feat: 회원정보(닉네임) 수정 및 비밀번호 수정 추가
    
    * refactor: 테스트 코드 삭제
    
    * refactor: 회원가입 오류 관련 수정
    
    * feat: 회원탈퇴 추가
    
    * chore: 소셜로그인 성공 url 설정

---
+++ src/test/java/com/ll/planify/PlanifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.planify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
